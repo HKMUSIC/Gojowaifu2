2eb9ae3bf60563cd3eb020c5b661408972e989ef
HKMUSIC <himanshu4hk9@gmail.com>
2025-10-25 15:25:49 +0530

    Update database.py

--- TEAMZYRO/modules/database.py
+++ TEAMZYRO/modules/database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB connection URI (heroku me set karo as env variable)
 import os
-MONGO_URI = os.getenv("MONGO_URI")  # set in Heroku config vars
+MONGO_URI = os.getenv("mongodb+srv://Gojowaifu2:Gojowaifu2@cluster0.uvox90s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # set in Heroku config vars
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["TEAMZYRO"]  # database name
