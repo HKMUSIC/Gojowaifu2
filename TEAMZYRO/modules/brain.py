0a33b15257aad45adb4aaf7eb7e996d51ac5ee70
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-02 09:48:49 +0530

    Create brain.py

---
+++ TEAMZYRO/modules/brain.py
@@ -0,0 +1,44 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import random
+
+@Client.on_message(filters.command("brain"))
+async def brain_cmd(client: Client, message: Message):
+
+    # User must tag (reply) someone
+    if not message.reply_to_message:
+        return await message.reply(
+            "Please reply to a user's message!\n\nExample:\nReply to someone and type:\n`/brain`"
+        )
+
+    target = message.reply_to_message.from_user
+    percentage = random.randint(1, 100)
+
+    # Output like screenshot
+    text = f"""
+🧠 **{target.first_name}'s Brain level:** {percentage}%
+"""
+
+    await message.reply(text)
+
+
+@Client.on_message(filters.command("looks"))
+async def looks_cmd(client, message: Message):
+
+    # user must reply/tag someone
+    if not message.reply_to_message:
+        return await message.reply(
+            "Please reply to a user's message!\n\nExample:\nReply to someone and type:\n`/looks`"
+        )
+
+    target = message.reply_to_message.from_user
+
+    # Random looks percentage
+    looks = random.randint(1, 100)
+
+    # Stylish output like KiddoBot
+    text = f"""
+😎 **{target.first_name}'s Looks level:** {looks}%
+"""
+
+    await message.reply(text)
