d4c7400da6b6cf2c61e658b830f4cef5a8c3eb66
HKMUSIC <himanshu4hk9@gmail.com>
2025-12-05 15:29:27 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -1,26 +1,31 @@
+import asyncio
 import importlib
 from TEAMZYRO import ZYRO, LOGGER, send_start_message
 from TEAMZYRO.modules import ALL_MODULES
 
 
-def main():
+async def main():
+
     # Load all modules
     for module_name in ALL_MODULES:
         importlib.import_module("TEAMZYRO.modules." + module_name)
 
     LOGGER("TEAMZYRO.modules").info("All modules loaded")
 
-    # Start Pyrogram bot
-    ZYRO.run()
+    # Start Pyrogram client
+    await ZYRO.start()
 
-    # Optional start message
+    # Send start message (ignore errors)
     try:
         send_start_message()
     except Exception as e:
         LOGGER("START").error(f"Start message error: {e}")
 
-    LOGGER("TEAMZYRO").info("BOT RUNNING SUCCESSFULLY")
+    LOGGER("TEAMZYRO").info("BOT RUNNING...")
+
+    # Idle (keep receiving updates)
+    await asyncio.Event().wait()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
